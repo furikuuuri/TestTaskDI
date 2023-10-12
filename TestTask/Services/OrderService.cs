ce3fbf1cb65e2dcc50e67dd6d5f1456a9335d938
PHOENIXIT\molentsevich <molentsevich@phoenixit.ru>
2023-10-12 12:53:26 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class OrderService:IOrderService
+    {
+        public OrderService(ApplicationDbContext context)
+        {
+            Context = context;
+        }
+        private ApplicationDbContext Context { get; }
+
+        public Task<Order> GetOrder()
+        {
+            return Context.Orders
+                 .OrderByDescending(order => order.Quantity* order.Price)
+                 .FirstOrDefaultAsync();
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return Context.Orders
+                .Where(order => order.Quantity>10)
+                .ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class UserService:IUserService
+    {
+        public UserService(ApplicationDbContext context)
+        {
+            Context = context;
+        }
+        private ApplicationDbContext Context { get; }
+        public Task<User> GetUser()
+        {
+
+            return Context.Users
+                .OrderByDescending(user => user.Orders.Count())
+                .FirstOrDefaultAsync();
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return Context.Users
+                .Where(user=>user.Status == Enums.UserStatus.Inactive)
+                .ToListAsync();
+        }
+
+       
+    }
+}
